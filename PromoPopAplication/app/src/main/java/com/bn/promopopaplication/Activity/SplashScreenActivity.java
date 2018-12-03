a53f03926cd88eacdde03b036fc9d9007f2841cb
Bruna Neves <brubsnev@gmail.com>
2018-12-03 02:12:20 -0200

    verificar se existe alguém logado no Hanlder

--- PromoPopAplication/app/src/main/java/com/bn/promopopaplication/Activity/SplashScreenActivity.java
+++ PromoPopAplication/app/src/main/java/com/bn/promopopaplication/Activity/SplashScreenActivity.java
@@ -1,14 +1,26 @@
 package com.bn.promopopaplication.Activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.bn.promopopaplication.DAO.ConfigurationFirebase;
+import com.bn.promopopaplication.Entity.Users;
 import com.bn.promopopaplication.R;
+import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -18,14 +30,42 @@ public class SplashScreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_splash_screen);
 
-        FirebaseAuth firebaseAuth = ConfigurationFirebase.getFirebaseAuthtication();
-        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                openMainActivity();
+
+                FirebaseAuth firebaseAuth = ConfigurationFirebase.getFirebaseAuthtication();
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+                if(firebaseUser != null){
+
+                    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    String id = firebaseUser.getUid();
+                    DatabaseReference ref = database.getReference("user/"+ id);
+
+                    ref.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+
+                            if(dataSnapshot.getValue() != null){
+                                openMainActivity();
+                            }else{
+                                Intent intent = new Intent(SplashScreenActivity.this, MainStoreActivity.class);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            System.out.println("The read failed: " + databaseError.getCode());
+                        }
+                    });
+                } else {
+                    openMainActivity();
+                }
+
             }
         }, 2000);
 
